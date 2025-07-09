4fb1ca3340e96f59869dde9b7f832a9098e00ad7
mrhamana <saugatkhatiwada80@gmail.com>
2025-07-09 23:08:50 +0545

    Initialized the Repo

---
+++ build/CMakeFiles/my_opengl_app.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_opengl_app.dir/link.d"
+  "CMakeFiles/my_opengl_app.dir/src/main.cpp.o"
+  "CMakeFiles/my_opengl_app.dir/src/main.cpp.o.d"
+  "my_opengl_app"
+  "my_opengl_app.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/my_opengl_app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
